85730f94422a9fd64a1051889627fa2b4bf84dd0
Mitsudani <mdsachetti@gmail.com>
2021-08-12 14:34:05 -0300

    Finished app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,56 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
+import Clima from './components/Clima';
+import Error from './components/Error';
 
 function App() {
+
+  // state del formulario
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: '',
+    pais: ''
+  });
+
+  const [consultar, guardarConsultar] = useState(false);
+  const [resultado, guardarResultado] = useState({});
+  const [error, guardarError] = useState(false);
+
+  // Extraer ciudad y pais
+  const {ciudad, pais} = busqueda;
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      if (consultar) {
+        const appId = '2271e42f9e419d4eae782cdb63169ad5';
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&APPID=${appId}`;
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        guardarResultado(resultado);
+        guardarConsultar(false);
+
+        // Detectar si la consulta arrojó un resultado
+        if (resultado.cod === "404") {
+          guardarError(true);
+        } else {
+          guardarError(false);
+        }
+      
+      }
+    } 
+    consultarAPI();
+    // eslint-disable-next-line
+  },[consultar])
+
+  let componente;
+  if (error) {
+    componente = <Error mensaje="No hay resultados" />
+  } else {
+    componente = <Clima
+                 resultado={resultado}
+                 />
+  }
+
   return (
     <Fragment>
       <Header
@@ -13,10 +61,14 @@ function App() {
         <div className="container">
           <div className="row">
             <div className="col m6 s12">
-              <Formulario />
+              <Formulario
+                busqueda={busqueda}
+                guardarBusqueda={guardarBusqueda}
+                guardarConsultar={guardarConsultar}
+              />
             </div>
             <div className="col m6 s12">
-              2
+              {componente}
             </div>
           </div>
         </div>
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
+import Error from './Error';
+import PropTypes from 'prop-types';
 
-const Formulario = () => {
+const Formulario = ({busqueda, guardarBusqueda, guardarConsultar}) => {
 
-    // state del formulario
-    const [busqueda, guardarBusqueda] = useState({
-        ciudad: '',
-        pais: ''
-    });
+    const [error, guardarError] = useState(false);
 
     // Extraer ciudad y pais
     const {ciudad, pais} = busqueda;
@@ -20,8 +18,26 @@ const Formulario = () => {
         });
     }
 
+    // Cuando el usuario da submit al formulario
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        // Validar
+        if (ciudad.trim() === '' || pais.trim() === '') {
+            guardarError(true);
+            return;
+        }
+        guardarError(false);
+
+        // Pasar al componente principal
+        guardarConsultar(true);
+    }
+
     return (
-        <form>
+        <form
+            onSubmit={handleSubmit}
+        >
+            {error ? <Error mensaje="Ambos campos son obligatorios" /> : null}
             <div className="input-field col s12">
                 <input
                     type="text"
@@ -50,8 +66,21 @@ const Formulario = () => {
                 </select>
                 <label htmlFor="pais">País: </label>
             </div>
+            <div className="input-field col s12">
+                <input
+                    type="submit"
+                    value="Buscar Clima"
+                    className="waves-effect waves-light btn-large btn-block yellow accent-4"
+                />
+            </div>
         </form>
     );
 }
- 
+
+Formulario.propTypes = {
+    busqueda: PropTypes.object.isRequired,
+    guardarBusqueda: PropTypes.func.isRequired,
+    guardarConsultar: PropTypes.func.isRequired
+}
+
 export default Formulario;
